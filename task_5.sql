8b9bd9324477e514f90fa236c64331b784caa0ef
retha-n <22retha@gmail.com>
2025-08-09 11:49:01 +0200

    initial commit

---
+++ task_5.sql
@@ -0,0 +1,17 @@
+import mysql.connector
+alx_book_store = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "my_password",
+        database = "alx_book_store"
+)
+
+mycursor = alx_book_store.cursor()
+USE alx_book_store;
+WHERE
+    TABLE_SCHEMA = 'alx_book_store'
+    TABLE_NAME = 'Customers';
+sql = INSERT INTO Customers (customer_id, customer_name, email, address)
+val = VALUES (1, Cole Baidoo, cbaidoo@sandtech.co, 123 Happiness Ave. )
+mycursor.execute(sql, val)
+alx_book_store.commit()
