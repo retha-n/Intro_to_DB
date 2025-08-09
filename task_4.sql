605d6b9be0785ba230ea546ba4ba887564e9b152
retha-n <22retha@gmail.com>
2025-08-09 11:25:45 +0200

    Add files via upload

---
+++ task_4.sql
@@ -0,0 +1,17 @@
+import mysql.connector
+alx_book_store = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "r20062501",
+        database = "alx_book_store"
+)
+SHOW DATABASES,
+USE alx_book_store;
+SELECT
+    COLUMN_NAME,
+    COLUMN_TYPE,
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'books';
